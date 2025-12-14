64a3d47567cad04a2ffd6380edaf0e8b3ee33124
Dan <dangrig@dgmbplittle.local>
2025-12-14 19:09:55 +0300

    poll 0 correctly treated as timeout event, Pfds elements memset to 0

--- server/server2.c
+++ server/server2.c
@@ -7,8 +7,7 @@
 int main(int argc, char **argv) {
 
     int listenFd, connFd;
-    socklen_t addrSize;
-    struct sockaddr_in servAddr, cliAddr;
+    struct sockaddr_in servAddr;
 
     char buffLogs[MSGMLEN];
 
@@ -34,22 +33,18 @@ int main(int argc, char **argv) {
     snprintf(buffLogs, MSGMLEN, "Starting... Server's IP Address: %s, Inbound Port: %u.\n", inputServIp, inputServPort);
     writeft(logFd, buffLogs, inputServIp);
 
-    
-
-    struct pollfd pFds[CLIENTCAP + 1];                                   /* set up poll() */
-    memset(&pFds, '\0', sizeof(pFds));
-    int rv0;
-
 
     if ((listenFd = getlsocket(inputServIp, inputServPort)) == -1) {        /* initialize the listening socket*/
         exit(EXIT_FAILURE);
     }                  
 
 
+    struct pollfd pFds[CLIENTCAP + 1];                                   /* set up poll() */
+    memset(&pFds, 0, sizeof(pFds));
+    
     pFds[0].fd = listenFd;
     pFds[0].events = POLLIN | POLLPRI;
     int curOnline = 0;
-
 
     int flags1 = fcntl(listenFd, F_GETFL, 0);
     if (fcntl(listenFd, F_SETFL, flags1 | O_NONBLOCK) == -1) {
@@ -58,38 +53,50 @@ int main(int argc, char **argv) {
     }
 
     b_socket curUsers[CLIENTCAP];
+    memset(&curUsers, 0, sizeof(curUsers));
 
     for ( ; ; ) {
-        int pollResult = poll(pFds, curOnline + 1, 500);\
+
+        for (int i = 0; i < CLIENTCAP; i++)
+        {
+            pFds[i].revents = 0;
+        }
+        int pollResult = poll(pFds, curOnline + 1, 1000);
 //        printf("pollResult: %d, curOnline: %d, curOnline fd: %d\n", pollResult, curOnline, pFds[curOnline].fd);
-        if (pollResult == -1) {
+        if (pollResult < 0) {
             perror("poll error\n");
 //            exit(EXIT_FAILURE);
         }
-        else if (pollResult >= 0) {
+        else if (pollResult == 0) {
+            continue;
+        }
+        else {
             if (pFds[0].revents & POLLIN) {                                                     /* check the listening socket for new connections */
 
                 char cliIpStr[INET_ADDRSTRLEN];
                 uint16_t cliPort;
                 char buffAux[64];
+                struct sockaddr_in cliAddr;
+                socklen_t addrSize = sizeof(cliAddr);
 
 
                 connFd = accept(listenFd, (struct sockaddr *) &cliAddr, &addrSize);
                 curOnline++;
-                curUsers[curOnline] = (b_socket) {.fd = connFd, .addr = cliAddr};
+                b_socket *pCurUser = &curUsers[curOnline];
+                *pCurUser = (b_socket) {.fd = connFd, .addr = cliAddr};
+                printf("connFd: %i\n", connFd);
+    
+                inet_ntop(AF_INET, (struct sockaddr *) &pCurUser->addr.sin_addr.s_addr, cliIpStr, INET_ADDRSTRLEN);
+                snprintf(buffLogs, MSGMLEN, "Accepted a connection from: %s:%u. Socket: %i.\n", cliIpStr, ntohs(pCurUser->addr.sin_port), connFd); //change to strcliip cliport
+                writeft(logFd, buffLogs, inputServIp);
+
                 pFds[curOnline].fd = connFd;
-                pFds[0].events = POLLIN | POLLPRI;
+                pFds[curOnline].events = POLLIN | POLLPRI;
                 flags1 = fcntl(connFd, F_GETFL, 0);
                 if (fcntl(connFd, F_SETFL, flags1 | O_NONBLOCK) == -1) {
                     perror("fcntl");
                     exit(EXIT_FAILURE);
                 }
-                printf("connFd: %i\n", connFd);
-                
-                inet_ntop(AF_INET, (struct sockaddr *) &curUsers[curOnline].addr.sin_addr.s_addr, cliIpStr, INET_ADDRSTRLEN);
-                snprintf(buffLogs, MSGMLEN, "Accepted a connection from: %s:%u. Socket: %i.\n", cliIpStr, ntohs(curUsers[curOnline].addr.sin_port), connFd); //change to strcliip cliport
-                writeft(logFd, buffLogs, inputServIp);
-
             }
 
 
@@ -120,15 +127,13 @@ int main(int argc, char **argv) {
                     
                     switch (n)
                     {
-                    case (-1):
+                    case -1:
                         break;
-                    case (0):
+                    case 0:
                         snprintf(cliActorStr, sizeof(cliActorStr), "%s:%u", cliIpStr, cliPort);
                         snprintf(buffLogs, MSGMLEN, "%s has disconnected.\n", cliActorStr);
                         writeft(logFd, buffLogs, cliActorStr);
-                        pFds[i].fd = 0;
-                        pFds[i].events = 0;
-                        pFds[i].revents = 0;
+                        memset(&pFds[i], 0, sizeof(pFds[i]));
                         curOnline--;
                         close(connFd);
                         break;
@@ -140,7 +145,7 @@ int main(int argc, char **argv) {
                     }
                     
                 }
-            }
+            } 
         }
 
     }
