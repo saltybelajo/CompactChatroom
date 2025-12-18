81f16a9270239a86d95deae6f2730f5421cc92b0
Dan <dn.grig2005@gmail.com>
2025-12-18 21:38:07 +0300

    fixed sigfault

--- client/c_aux.c
+++ client/c_aux.c
@@ -7,21 +7,23 @@ int anm_deconstruct_msg(char *parcel, char *allocAuthor, char *allocPayload) {
     uint8_t parcelSize = parcel[0];
     char *parcelCopy = malloc(parcelSize);
     strncpy(parcelCopy, parcel, parcelSize);
+    char *mallocTarget0 = parcelCopy;
     parcelCopy += 1;
 
     uint8_t authorSize = 0;
-    for (int i = 0; parcelCopy[i] != '^' || i < parcelSize; i++) {
+    for (int i = 0; parcelCopy[i] != '^' && i < parcelSize; i++) {
         allocAuthor[i] = parcelCopy[i];
         authorSize++;
     }
-    allocAuthor[authorSize - 1] = 0;
+    allocAuthor[authorSize] = 0;
 
     parcelCopy += authorSize + 1;
     uint8_t payloadSize = 0;
-    for (int i = 0; parcelCopy[i] != 0 || i < parcelSize; i++) {
+    for (int i = 0; parcelCopy[i] != 0 && i < parcelSize; i++) {
         allocPayload[i] = parcelCopy[i];
         authorSize++;
     }
+    free(mallocTarget0);
 
-    return(1);
+    return 1;
 }
