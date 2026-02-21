1fe7810367906787c9df1d1ff9c621d6f159d0c0
Dan <dn.grig2005@gmail.com>
2026-02-21 15:20:49 +0300

    reorganizing

--- client/client.c
+++ client/client.c
@@ -282,7 +282,7 @@ int main(int argc, char **argv) {
                 printf("Incorrect command entered.\n");
             }
 
-            free(cmdIndex);
+            free(&cmdIndex);
         }
         
         
